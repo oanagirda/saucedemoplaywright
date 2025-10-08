e74394dcf9a248ed025435d7290704930e321f36
Oana Girda <oanagirda@192.168.1.7>
2025-10-08 10:17:20 +0300

    Added POM structure and comments  with Login, Inventory, Cart and Checkout pages

--- playwright.config.js
+++ playwright.config.js
@@ -4,13 +4,13 @@ const { defineConfig } = require('@playwright/test');
 module.exports = defineConfig({
   use: {
     baseURL: 'https://www.saucedemo.com',
-    headless: false,
+    headless: true,
     screenshot: 'only-on-failure',
     trace: 'on-first-retry',
     video: 'retain-on-failure',
-    launchOptions: {
-      slowMo: 1500,     // slows down every action with 1500 ms
-    },
+    //launchOptions: {
+      //slowMo: 1500,     // slows down every action with 1500 ms
+    //},
     viewport: { width: 1280, height: 800 },
   },
   retries: 1,
